c64b1b9b11efe49b7ee85bd13e85b42b990fcf3b
Yoko <yokoujihara622@gmail.com>
2022-07-11 16:54:40 +1000

    fixed api error

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,32 +1,39 @@
 const searchForm = document.getElementById("search-form");
 const inputSearch = document.getElementById("input-search");
-
+const currentDayCity =document.getElementById("current-day-city");
+const currentDayTemp =document.getElementById("current-day-temp");
+const currentDayWind =document.getElementById("current-day-wind");
+const submitButton = document.getElementById("submit-button");
+const currentDayHumidity =document.getElementById("current-day-humidity");
+const currentDay = moment().format("dddd, MMMM Do YYYY, h:mm:ss a")
 const APIkey = "a5186e357cf6ed9768a0dc421bcc6e3b";
 
 function getOneCallApi(lon, lat) {
   
-  const oneCallUrl = "";
-  fetch(oneCallUrl)
+  const oneCallUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${APIkey}`;
+  return fetch(oneCallUrl)
+    .then(function(res){
+      return res.json();
+    })
+    
 }
+
 
 function getWeatherData(city) {
   
   const queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIkey;
-  fetch(queryURL)
+  return fetch(queryURL)
    .then(function(response){
      return response.json();
    })
    .then(function(currentWeather){
 
-    console.log(currentWeather);
-    return getOneCallApi(lon, lat);
+    // console.log(currentWeather);
+    return getOneCallApi(currentWeather.coord.lon, currentWeather.coord.lat, currentWeather.dt);
    })
 }
 
-
-
-//  when user clicks on the search btn
-searchForm.addEventListener('click', function(event){
+function fetchWeatherData(event){
   event.preventDefault();
   
   // get user input
@@ -35,12 +42,31 @@ searchForm.addEventListener('click', function(event){
   // send req to weather API
   
   // fetch weather data based on city name
+  // Promises
   getWeatherData(userInput)
+  .then(function(weatherData){
+    console.log(weatherData);
 
-  // once got the data 
-  // populate the ddata into te dom
+    // once got the data 
+    // populate the data into the dom
 
-  // store the city name into localstorage
-  // render the history into the serach list
-});
+    // current
+    const datetime = moment().format("YYYY-MM-DD");
+    console.log(datetime)
+
+    currentDayCity.innerHTML = `${userInput} ${datetime} icon`;
+    currentDayTemp.textContent = weatherData.current.temp + "k";
+    currentDayHumidity.textContent = weatherData.current.humidity
+    currentDayWind.textContent = weatherData.current.wind-speed + "kmh"
+  
+    // store the city name into localstorage
+    // render the history into the serach list
+  })
+
+}
+
+
+//  when user clicks on the search btn
+searchForm.addEventListener('submit', fetchWeatherData);
+submitButton.addEventListener('click', fetchWeatherData);
 
